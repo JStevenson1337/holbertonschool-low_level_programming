cce8d62a772ac94df74e8e34de159e49a9c8b501
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-09-27 17:12:07 -0500

    Updating Tracker and Sanity Check

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*
+ *  Write a C program that prin
+ * ts exactly "Programming is like building
+ * a multilingual puzzle, followed by a new line.
+ */
+/**
+ * main - Void
+ *
+ Return: String
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/*
+ *Write a C program that prints exactly with
+ *proper grammar, but the outcome is a
+ *piece of art,, followed by a new line.
+*/
+
+/**
+ *  main - Entry point
+ * Return:  0 returns the string
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
