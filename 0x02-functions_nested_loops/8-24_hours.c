86dd5a7266732830e848ba4b0bc43fc249c9723e
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-09-29 22:33:40 -0500

    Updated"

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,28 +7,26 @@
  */
 void jack_bauer(void)
 {
-	int h1, h2, m1, m2;
+int h1, h2, m1, m2;
 
-	for (h1 = '0'; h1 <= '2'; h1++)
-	{
-		for (h2 = '0'; h2 <= '4'; h2++)
-			{
-				for (m1 = '0'; m1 < '6';  m1++)
-				{
-					for (m2 = '0'; m2 <= '9'; m2++)
-					{
-
-						_putchar(h1);
-						_putchar(h2);
-						_putchar(':');
-						_putchar(m1);
-						_putchar(m2);
-						_putchar('\n');
-				}
-			}
-		}
-		if (h1 == 2 && h2 == 3)
-			break;
-	}
-
+for (h1 = '0'; h1 <= '2'; h1++)
+{
+for (h2 = '0'; h2 <= '9'; h2++)
+{
+for (m1 = '0'; m1 < '6';  m1++)
+{
+for (m2 = '0'; m2 <= '9'; m2++)
+{
+_putchar(h1);
+_putchar(h2);
+_putchar(':');
+_putchar(m1);
+_putchar(m2);
+_putchar('\n');
+}
+}
+}
+if (h1 == 2 && h2 == 3)
+break;
+}
 }
