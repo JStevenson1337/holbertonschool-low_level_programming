3cf8b84d28b4a31af1de7ccfb3478a635cf592a9
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-10-01 13:08:43 -0500

    cleaning folders

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *
+ * add - add to integers
+ *
+ * Return: Result r
+ */
+int add(int i1, int i2)
+{
+	int r1;
+
+	r1 = i1 + i2;
+	return (r1);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include<stdio.h>
+int main()
+{
+	    int i1, i2, r;
+		    for ( x = 0; x < 10; x++  )
+			{
+				    /*    _putchar(',');*/
+				        for ( y = 0; y < 10; y++  )
+						{
+							            (r = x * y);
+										            putchar(r%10 + '0');
+													            putchar(' ');
+																            putchar(',');
+
+						}
+						      putchar('\n');
+
+
+
+			}
+			    return (0);
+
+}
