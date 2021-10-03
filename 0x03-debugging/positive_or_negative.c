80859f94df3005c581f44e4baf31363898924050
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-03 18:14:07 -0500

    checking 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include<stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - generates a random number  and assigns
+ * it to the variable n
+ *
+ * Return: n and evaluates against logical operators
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is positive\n", n);
+	}
+	return (0);
+}
