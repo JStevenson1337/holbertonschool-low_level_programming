8a22077a42c117d8e577d703008ebf733d0a2c60
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-04 10:56:41 -0500

    Removrd whitespace on 0 and 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,11 +4,12 @@
  * Return: 0
  */
 
-int main(void) {
-  int i;
+int main(void)
+{
+	int i;
 
-  i = 0;
-  positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-  return (0);
+	return (0);
 }
