7a846b3522be225ff4db46c8c16490f0003ed3fa
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-04 12:15:09 -0500

    corrected betty syntax

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main - tests function that prints if integer is positive or negative
+ * postitive_or_negative - tests function that
+ * prints if integer is positive or negative
  * Return: 0
  */
 
-int main(void)
+
+int postitive_or_negative(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
-
 	return (0);
 }
