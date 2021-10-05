776d8f8e6dc8a1b7aeeb462f80923a2fe27c936b
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-04 20:39:10 -0500

    Tacking 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - entry
+ *
+ * Description: Multiplies 2 number returns the result
+ * @a: first int
+ * @b: second int
+ * Returns: result of a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
