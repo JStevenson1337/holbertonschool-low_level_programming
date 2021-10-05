615abf8d73d3e8a9755b4808a3102b97ca35fed4
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-05 00:32:17 -0500

    cleaning repository

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include "main.h"
+
 /**
- * prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
-
-int postitive_or_negative(void)
+int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+i = 0;
+positive_or_negative(i);
+
+return (0);
 }
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,13 @@
+#include "main:w
+
+
+
+int main(void)
+{
+	        int i;
+
+		        i = 98;
+			        positive_or_negative(i);
+
+				        return (0);
+}
