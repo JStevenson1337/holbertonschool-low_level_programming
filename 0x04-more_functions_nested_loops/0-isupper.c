ac5ab5707265234629ee9dc7d16a577968374978
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-04 21:02:19 -0500

    Corrected Betty Syntax

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * _isupper -checks for upper case chars
+ * _isupper - Checks for uppercase letters
+ * Description: checks for upper case chars
  * @c: vairable to test against bool
- * Returns: 1 if c uppercase
+ * Return: 1 if true else 0
  */
-int _isupper(int c)
+int _isupper(int c)	/* Checks for an uppercase char */
 {
 	if (c >= 'A' && c <= 'Z')
 	{
