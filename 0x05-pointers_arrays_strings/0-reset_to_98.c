908662aff53f0c437f03e6ba169f130fcee4c508
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-05 08:36:13 -0500

    adding PS 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *  reset_to_98 - function start
+ *  Description: Instanciates a pointer
+ *  @n: pointer to be created
+ *  Return: *n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
