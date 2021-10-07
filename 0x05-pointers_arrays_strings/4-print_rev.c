2495a3139c2438ae028144e4059bd15a38785054
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-06 23:13:15 -0500

    add 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - Function Start
+ *	Description: Prints Text passed in from str
+ *	@str: stringpointer  passed into function
+ * Return: Void
+ */
+
+void print_rev(char *str)
+{
+	while (*str != '\0')
+	{
+		str++;
+	}
+	str--;	
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str--;
+	}
+	_putchar('\n');
+}
