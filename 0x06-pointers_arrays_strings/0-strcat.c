cf5d42cf178e3ca39fe274be24daac1eee9c4ebc
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-10 15:28:05 -0500

    Adding 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strcat - append a strin to another string
+ * @src: source srting(s)
+ * @dest: Destination String
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int start = 0;
+
+	while (*dest)
+	{
+		dest++;
+		start++;
+	}
+	while (*src)
+	{
+		src++;
+		start++;
+	}
+	*(dest + 1) = '\0';
+	return (dest);
+}
