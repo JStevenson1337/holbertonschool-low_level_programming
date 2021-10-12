aa51b4c5dcf936f5f57cc4734d192b754f5c9e78
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2021-10-11 23:49:38 -0500

    corrected betty syntax

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,6 +5,7 @@
  * @b: constant bytes
  * @n: number of bytes
  * Decsription: function that fills memory with a constant byte.
+ * Return: mutated string
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -13,7 +14,7 @@ char *_memset(char *s, char b, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		s[i] = b;
 	}
 
 	return (s);
