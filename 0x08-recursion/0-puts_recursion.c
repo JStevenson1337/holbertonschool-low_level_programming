6fac0cbc15fecfbf12ce10fdb3ab12e7b00cce74
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-10-12 19:07:50 -0500

    corrected betty syntax

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == 1'\0')
-	{
-	 _putchar('\n');
-	}
+	if (*s == '\0')
+		_putchar('\n');
 	else
 	{
 		_putchar(*s);
 		_puts_recursion(++s);
 	}
-
 }
 
