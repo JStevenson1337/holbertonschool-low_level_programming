1423ac2832ba3ac9e5090e4079066aae553b545a
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-10-15 23:14:15 -0500

    correcting 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,19 +1,37 @@
 #include "main.h"
-int _memcmp(const void *s1, const void *s2, int len);
-int string_length(char s[]);
 /**
- * _strstr - start
- * @haystack: search space
+ * _strstr - function start
+ * @haystack:input string
  * @needle: search criteria
- * Description:
- * Return:
+ * Description:find substring in string
+ * Return:Result
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i;
+	int i;
+	int j;
 
-	for (i = 0; *(haystack + i) != '\0'; i++)
-		if (*needle, (haystack + i))
-			return (haystack + i);
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] == '\0')
+					break;
+				if (needle[j] == haystack[i + j])
+					continue;
+				break;
+			}
+			if (needle[j] == '\0')
+			{
+				haystack += i;
+				return (haystack);
+			}
+		}
+	}
 	return (0);
 }
+
