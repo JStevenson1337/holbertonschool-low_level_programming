d5c4241f76a9d7c41cf4bc50c3ee553e9cc98a85
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-10-20 14:55:55 -0500

    corrected 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || ptr == NULL)
 		return (NULL);
 
-	for (c[i] = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
