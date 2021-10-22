f5e28ad292c44da0deaf61ebe2ebfe0e60aa4eed
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-10-21 22:14:13 -0500

    betty correction

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,11 +1,11 @@
 #ifndef FUNCTION_LIKE_MACRO_H
-#include <stdio.h>
 #define FUNCTION_LIKE_MACRO_H
+#include <stdio.h>
 #define ABS(x) ((x < 0) ? (-x) : (x))
 /*
-#define ABS(x) ({ \
-    typeof(x) x = (x); \
-    x < 0 ? -x : x ; \
-    })
-*/
+ * #define ABS(x) ({ \
+ * typeof(x) x = (x); \
+ *   x < 0 ? -x : x ; \
+ *  })
+ */
 #endif
