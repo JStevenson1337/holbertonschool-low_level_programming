5680ab285a5a3015cdbee3dcf23ff8745201681e
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-10-26 13:33:29 -0500

    Beginning  new project

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct
+ * Description: define dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
