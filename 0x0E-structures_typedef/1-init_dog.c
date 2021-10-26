95d57535275261f2afe9c414215e61abffc642f6
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-10-26 14:17:30 -0500

    correcting 1 adding 2

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * init_dog - Initialize the dog
  * @d pointer to the dog
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/** @brief Prints a dog
+ *
+ * @param dog The dog to print
+ * @return void
+ *
+ */
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\nAge: %f\nOwner: %s ", d->name, d->age , d->owner);
+}
+
