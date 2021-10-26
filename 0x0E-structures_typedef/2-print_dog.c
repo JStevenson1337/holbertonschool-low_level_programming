6f8e5220dd22885e160fc2d14b587dce040531a9
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-10-26 18:28:15 -0500

    correcing output

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,6 +9,5 @@
  */
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\nAge: %f\nOwner: %s", d->name, d->age , d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age , d->owner);
 }
-
