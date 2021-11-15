036da731074d64c863c1b37f50ed1503fc17399e
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-11-15 14:42:20 -0600

    correcting 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,57 +1,30 @@
 #include "main.h"
+unsigned int counting_bits(unsigned int a);
 /**
- * @brief 
- * 
- * 
+ * flip_bits - a function that returns the number of bits/
+ * you would need to flip to get from one number to another
+ * @n:first num
+ * @m: second num
+ * Return: number of bits
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-
-int countNums(unsigned int n);
-/**
- * print_binary - prints the binary representation of a number
- * @n: number to be printed
- * Return: void
- */
-void print_binary(unsigned long int n)
-{
-	int i;
-
-	int count = countNums(n);
-
-	if (n == 0)
-	{
-		printf("0");
-	}
-	else
-	{
-		for (i = count - 1; i >= 0; i--)
-		{
-			if (n & (1 << i))
-			{
-				printf("1");
-			}
-			else
-			{
-				printf("0");
-			}
-		}
-	}
+	return (counting_bits(n ^ m));
 }
-
 /**
- * countNums - counts the number of bits in a number
- * @n: number to be counted
- * Return: number of bits
+ * counting_bits - counts the number of bits
+ * @a: integer
+ * Return: Count
  */
-int countNums(unsigned int n)
+unsigned int counting_bits(unsigned int a)
 {
 	int count = 0;
 
-	while (n)
+	while (a > 0)
 	{
 		count++;
-		n >>= 1;
+		a &= (a - 1);
 	}
 	return (count);
 }
+
