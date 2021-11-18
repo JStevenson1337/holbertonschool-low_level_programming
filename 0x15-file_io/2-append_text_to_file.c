08163ad28cbd31e0e92933c932d486bc51489a87
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-11-18 09:59:27 -0600

    correctionss made

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * @brief append_text_to_file
- * @param file_name
- * @param text
+ * @filename: file_name
+ * @text_content: text
  * @return
  */
 int append_text_to_file(const char *filename, char *text_content)
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
