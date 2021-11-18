e9ad5e027fde55ce0055c6bac1c1424db1707f0a
Jeremy Stevenson <jeremy.m.stevenson@gmail.com>
2021-11-18 09:26:36 -0600

    corrected 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -42,24 +42,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 
 
-// {
-// 	int file, rd, wr;
-// 	char *buf;
 
-// 	if (filename == NULL)
-// 		return (0);
 
-// 	file = fopen(filename, O_RDONLY);
-
-// 	if (file == -1)
-// 		return (0);
-
-// 	buf = malloc(sizeof(char) * letters + 1);
-// 	if (buf == NULL)
-// 		return (0);
-
-// 	buf = malloc(sizeof(char) * letters + 1);
-// 	if(buf == NULL)
-// 		return (0);
-	
-// }
