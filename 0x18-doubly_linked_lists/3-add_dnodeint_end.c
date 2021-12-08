adc1940540eba6cad6f70668d59a67b17d95dfbb
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-12-07 19:03:17 -0600

    corrected iteration

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,7 +7,10 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
+
 	dlistint_t *new_node;
+	dlistint_t *temp;
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -15,11 +18,13 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 	if (*head == NULL)
 	{
-		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
-	new_node->prev = (*head)->prev;
-	(*head)->prev = new_node;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new_node;
 	return (new_node);
 }
+
