b8dbf569f1178e891e2643b75f0ff37e8bbfebc3
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-12-08 18:29:36 -0600

    corrected betty syntax

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -1,20 +1,24 @@
 #include "lists.h"
 /**
- * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * add_dnodeint - adds a new node at the end of a dlistint_t list
  * @head: pointer to the head of the list
  * @n: integer to be added
  * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = *head;
-	new_node->prev = NULL;
-	if (*head != NULL)
-		(*head)->prev = new_node;
-	*head = new_node;
-	return (new_node);
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));	/* create new node */
+
+	if (new_node == NULL)	/* if malloc fails */
+		return (NULL);	/* return NULL */
+
+	new_node->n = n;	/* assign the value to the new node */
+	new_node->next = *head;	/* assign the next node to the head */
+	new_node->prev = NULL;	/* assign the prev node to NULL */
+
+	if (*head != NULL)	/* if the list is not empty */
+		(*head)->prev = new_node;	/* assign the prev node to the head */
+
+	*head = new_node;	/* assign the new node to the head */
+	return (new_node);	/* return the address of the new node */
 }
