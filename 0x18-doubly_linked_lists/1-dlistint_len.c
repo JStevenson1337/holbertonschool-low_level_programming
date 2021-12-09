7229b29c69743bac27a2b677c9cbdabe5ea23f66
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-12-08 18:25:30 -0600

    corrected betty

--- 0x18-doubly_linked_lists/1-dlistint_len.c
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
  * dlistint_len - returns the number of elements in a dlistint_t list
- * *h: pointer to the head of the list
+ * @h: pointer to the head of the list
  * Return: number of elements in the list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t len = 0;
+
 	while (h)
 	{
 		h = h->next;
 		len++;
-
 	}
 	return (len);
 }
