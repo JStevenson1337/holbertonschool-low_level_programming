b659652f272f2e2159df4554a4c35442b7c6ea57
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-12-08 19:18:49 -0600

    Corrected requirment check

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,6 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 	if (*head == NULL)
 	{
+		new_node->prev = NULL;
 		*head = new_node;
 		return (new_node);
 	}
@@ -25,6 +26,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	while (temp->next != NULL)
 		temp = temp->next;
 	temp->next = new_node;
+	new_node->prev = temp;
+
 	return (new_node);
 }
 
