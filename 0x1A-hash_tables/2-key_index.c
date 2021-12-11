89c7ba358255c00bf28af50ad07789e2c181419b
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-12-11 11:43:50 -0600

    adding 2 and 3

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash = 0;
+	unsigned long int i = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		hash += key[i];
+	}
+
+	return (hash % size);
+}
