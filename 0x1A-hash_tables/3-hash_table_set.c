85883052fca7ce72687926775cc6ab34d40181d1
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-12-12 14:31:16 -0600

    corrections mad on 1 and 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,8 +18,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
 		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+	new_node->key = _strdup(key);
+	new_node->value = _strdup(value);
 	new_node->next = NULL;
 	if (!new_node->key || !new_node->value)
 	{
@@ -38,4 +38,37 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		temp = temp->next;
 	temp->next = new_node;
 	return (1);
+}
+
+char _strdup(char *str)
+{
+	int len = _strlen(str);
+	char *dupstr = malloc((len) * sizeof(char));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	if (dupstr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		dupstr = (char *) _memcpy(dupstr, str, len);
+	}
+	return (dupstr);
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *csrc = (char *)src;
+	char *cdest = (char *)dest;
+
+	for (i = 0; i < n; i++)
+	{
+		cdest[i] = csrc[i];
+	}
+	return (dest);
 }
