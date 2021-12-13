450dfb5897264867f7846b3ac60b0da820f9b400
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-12-12 23:33:02 -0600

    corrections

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,12 +3,9 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash = 0;
-	unsigned long int i = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		hash += key[i];
-	}
+	hash = hash_djb2(key) % size;
 
-	return hash % size;
+	return (hash);
+	
 }
