ffdfd393f4c30923ca573fa8cff666fbfc27c494
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-12-12 23:42:12 -0600

    corrected free logic

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,31 +7,16 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int index;
-	hash_node_t *temp;
-	hash_node_t *prev;
-
-	if (ht == NULL || ht->size == 0)
+	if (!ht)
 		return;
-	index = key_index((const unsigned char *)ht->array, ht->size);
-	temp = ht->array[index];
-	prev = NULL;
-	while (temp != NULL)
-	{
-		if (strcmp(temp->key, (const char*)(ht->array[index])) == 0)
-		{
-			if (prev == NULL)
-				ht->array[index] = temp->next;
-			else
-				prev->next = temp->next;
-			free(temp->key);
-			free(temp->value);
-			free(temp);
-			ht->size--;
-			return;
-		}
-		prev = temp;
-		temp = temp->next;
-	}
 
+	while (ht->size > 0)
+	{
+		free(ht->array[ht->size]->key);
+		free(ht->array[ht->size]->value);
+		ht->size--;
+	}
+	free(ht->array);
+	free(ht);
+	
 }
