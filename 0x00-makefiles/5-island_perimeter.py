715ef82b46669a54585c5fbeb27f16a904f85d7f
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-05-15 15:13:42 -0500

    fix files

---
+++ 0x00-makefiles/5-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Calculates the perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of an island.
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                if i == len(grid)-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                if j == len(grid[i])-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+    return perimeter
