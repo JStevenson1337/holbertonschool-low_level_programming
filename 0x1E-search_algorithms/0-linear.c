8427cb46e01cf641887b44f0eb0f838dec0d670b
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-06-13 18:48:16 -0500

    add: linear search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+/**
+ * linear_search - search elements 1 by 1
+ * @array: pointer to array
+ * @size: is the number of elements in array
+ * @value: search item
+ *
+ * Return: index of value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = %d\n", i, array[i]);
+		if (array[i] == value)
+		return (i);
+	}
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __ALGOS_H__
+#define __ALGOS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+
+
+#endif /* __ALGOS_H__ */
