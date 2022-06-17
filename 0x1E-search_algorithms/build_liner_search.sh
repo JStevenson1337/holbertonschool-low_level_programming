7f0c307c054b663c3a1960d6303db18231bafbca
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-06-16 23:24:26 -0500

    update

---
+++ 0x1E-search_algorithms/build_liner_search.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gcc -Wall -Wextra -Werror -pedantic -std=gnu89 0-main.c 0-linear.c -o 0-linear
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,6 @@
 #include <stddef.h>
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 
 #endif /* __ALGOS_H__ */
