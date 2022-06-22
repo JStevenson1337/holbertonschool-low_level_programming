212018da5b444e1af44f85dfcf0d3dbe50c0a655
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-06-22 15:47:22 -0500

    Add: prototype for task 104, created build script

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,17 @@
+#include "search_algos.h"
+/**
+ * advanced_binary - advanced_binary sorting algorithm
+ * @array: array of integers
+ * @size: size of array
+ * @value: value to sort by
+ * Return: Value or -1 if not found or NULL
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+    if (array == NULL)
+        return -1;
+
+
+
+
+}
