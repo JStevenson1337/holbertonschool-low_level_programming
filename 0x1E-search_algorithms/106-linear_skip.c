cc2f83b6e649f1287a8d9b01ab6d249758ce5e95
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-06-28 13:22:21 -0500

    initial commit for task 106

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,10 @@
+#include "search_algos.h"
+/**
+ * 
+ * 
+ * 
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,4 +11,24 @@ int binary_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int ID_Val(int *array, int left, int right, int value);
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
 #endif /* __ALGOS_H__ */
