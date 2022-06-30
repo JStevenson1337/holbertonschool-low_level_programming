e11b3c7886f2028100e5cec16c3e5aba9f5dd91d
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-06-29 19:17:47 -0500

    test

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -7,5 +7,14 @@
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-
+    if (!list)
+        return (NULL);
+    
+    while (list)
+    {
+        if (list->n == value)
+            return (list);
+        list = list->next;
+    }
+    return (NULL);
 }
