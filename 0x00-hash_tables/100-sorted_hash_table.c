27f149537822654346a386ba59cffc0303194235
jstevenson1337 <jmstevenson918@gmail.com>
2023-02-12 11:13:25 -0600

    fixed indentation

--- 0x00-hash_tables/100-sorted_hash_table.c
+++ 0x00-hash_tables/100-sorted_hash_table.c
@@ -8,22 +8,17 @@
 
 shash_table_t *shash_table_create(unsigned long int size)
 {
-	shash_table_t *table;
-	
+    shash_table_t *table;
 
-  if (!size)
-          return (NULL);
-
-	if ((table = malloc(sizeof(shash_table_t))) == NULL)
-          return (NULL);
-
-	if ((table -> array = malloc(sizeof(shash_table_t) * size)) == NULL)
-          return (NULL);
-
-  table -> shead = NULL;
-  table -> stail = NULL;
-  return (table);
-
+    if (!size)
+        return (NULL);
+    if ((table = malloc(sizeof(shash_table_t))) == NULL)
+        return (NULL);
+    if ((table -> array = malloc(sizeof(shash_table_t) * size)) == NULL)
+        return (NULL);
+    table -> shead = NULL;
+    table -> stail = NULL;
+    return (table);
 }
 // int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 // The key/value pair should be inserted in the sorted list at the
@@ -33,13 +28,13 @@ shash_table_t *shash_table_create(unsigned long int size)
 // that we can print by traversing it. See example.
 int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 {
-	size_t ptr1, ptr2, ptr3;
-	int i = 0;
+    size_t ptr1, ptr2, ptr3;
+    int i = 0;
 
-	shash_table_t *table = malloc(sizeof(shash_table_t)); /* allocate table and set pointer to the beginning of the table */
-	ptr1 = (size_t)table[ptr2];
-	shash_node_t *array = malloc(sizeof(shash_node_t[ptr2]));
-	ptr3 =  
+    shash_table_t *table = malloc(sizeof(shash_table_t)); /* allocate table and set pointer to the beginning of the table */
+    ptr1 = (size_t)table[ptr2];
+    shash_node_t *array = malloc(sizeof(shash_node_t[ptr2]));
+    ptr3 =  
 
 
 
@@ -71,29 +66,28 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value)
 // TODO: Document Print Function
 void shash_table_print(const shash_table_t *ht)
 {
-	size_t *curr, **array, *curr_head;
-	int i, *j;
+    size_t *curr, **array, *curr_head;
+    int i, *j;
 
-  if (!ht)
-          return;
+    if (!ht)
+        return;
 
-	*curr = (size_t)ht -> shead;
+        *curr = (size_t)ht -> shead;
 
-  while (curr)
-  {
-          printf("'%s': '%s'\n", curr -> key, curr -> value);
+    while (curr)
+    {
+        printf("'%s': '%s'\n", curr -> key, curr -> value);
 
-          curr = curr -> snext;
-
-  }
+        curr = curr -> snext;
+    }
 }
 
 int main(void)
-{ 
-	shash_table_t *ht;
+{
+    shash_table_t *ht;
 
-	ht = shash_table_create(1024);
-	shash_table_set(ht, "y", "0");
+    ht = shash_table_create(1024);
+    shash_table_set(ht, "y", "0");
     shash_table_print(ht);
 
     return (EXIT_SUCCESS);
